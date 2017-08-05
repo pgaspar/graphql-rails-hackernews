15ba5e3a1c7bacf7197e917e20774bd1ccd429d4
Pedro Gaspar <pedro.gaxpar@gmail.com>
2017-08-05 13:53:17 +0100

    Add votes and links to UserType

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,4 +4,6 @@ Types::UserType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :name, !types.String
   field :email, !types.String
+  field :links, -> { !types[Types::LinkType] }
+  field :votes, -> { !types[Types::VoteType] }
 end
