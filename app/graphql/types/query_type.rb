d08bf4ee266bcd6e3f460bb92cf493fc5665dea9
Pedro Gaspar <pedro.gaxpar@gmail.com>
2017-08-05 13:56:35 +0100

    Add root-level allUsers field

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,4 +6,8 @@ Types::QueryType = GraphQL::ObjectType.define do
   field :allLinks, !types[Types::LinkType] do
     resolve -> (obj, args, ctx) { Link.all }
   end
+
+  field :allUsers, !types[Types::UserType] do
+    resolve -> (obj, args, ctx) { User.all }
+  end
 end
