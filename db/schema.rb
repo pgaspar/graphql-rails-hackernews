c3f74511dec8dbfb04e63bddf489cfc0d631dde2
Pedro Gaspar <pedro.gaxpar@gmail.com>
2017-08-05 12:03:45 +0100

    Add Vote model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170803015541) do
+ActiveRecord::Schema.define(version: 20170803114757) do
 
   create_table "links", force: :cascade do |t|
     t.string "url"
@@ -27,6 +27,15 @@ ActiveRecord::Schema.define(version: 20170803015541) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "link_id"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["link_id"], name: "index_votes_on_link_id"
+    t.index ["user_id"], name: "index_votes_on_user_id"
   end
 
 end
