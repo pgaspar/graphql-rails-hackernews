c8dcbc26b584f53f8693345c00505960a016f520
Pedro Gaspar <pedro.gaxpar@gmail.com>
2017-08-05 13:48:36 +0100

    Update Seeds file to create valid Links

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+# Generate a user
+user = User.create name: 'John', email: 'johndoe@gmail.co', password: 'password'
+
 # Generate a couple links
-Link.create url: 'http://graphql.org/', description: 'The Best Query Language'
-Link.create url: 'http://dev.apollodata.com/', description: 'Awesome GraphQL Client'
+Link.create url: 'http://graphql.org/', description: 'The Best Query Language', user: user
+Link.create url: 'http://dev.apollodata.com/', description: 'Awesome GraphQL Client', user: user
